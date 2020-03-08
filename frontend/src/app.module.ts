9f34408632447b8c8dc67e5b399ed0760b130d83
T'Avvion Jones <43850711+Halwahadi@users.noreply.github.com>
2020-03-08 17:55:26 -0400

    Add files via upload

---
+++ frontend/src/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginSignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
