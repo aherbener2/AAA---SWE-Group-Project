dd9bdfea1ad84ba1240954ad62dcb977955bdb1c
Will Lyons <wdlyons1@gmail.com>
2020-03-22 11:09:41 -0400

    Add files via upload

--- src/main/java/edu/gsu/swe/aaaapp/Tutor.java
+++ src/main/java/edu/gsu/swe/aaaapp/Tutor.java
@@ -1,61 +1,85 @@
 package edu.gsu.swe.aaaapp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tutor")
 public class Tutor {
-	private int Pantherid;
-	private String username;
+	private String Pantherid;
+//	private String username;
 	private String password;
-	private String name;
+	private String firstName;
+	private String lastName;
 	private String email;
-	private String courses;
-	
-	public Tutor(String username, String password, String name, String email, int pantherid) {
-	this.username = username;
-	this.password = password;
-	this.email = email;
-	this.name = name;
-	this.Pantherid = pantherid;
+//	private String courses;
+
+	public Tutor(String password, String firstName, String lastName, String email, String pantherid) {
+//	this.username = username;
+		this.password = password;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.Pantherid = pantherid;
 	}
-	public void setid(int id) {
+
+	public void setid(String id) {
 		this.Pantherid = id;
 	}
-	public int getid() {
+
+	@Column(name = "lastName", nullable = false)
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Column(name = "pantherId", nullable = false)
+	public String getid() {
 		return Pantherid;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getEmail(){
+
+	@Column(name = "email", nullable = false)
+	public String getEmail() {
 		return email;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setUsername(String username){     
-	  this.username = username;    
-	}    
-	public String getUsername(){     
-	  return username;    
-	}   
 
-	public void setPassword(String password){     
-	  this.password= password;    
-	}    
-	public String getPassword(){     
-	  return password;    
+	public void setFirstName(String name) {
+		this.firstName = name;
 	}
-	
+
+	@Column(name = "firstName", nullable = false)
+	public String getFirstName() {
+		return firstName;
+	}
+//	public void setUsername(String username){     
+//	  this.username = username;    
+//	}    
+//	public String getUsername(){     
+//	  return username;    
+//	}   
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
 	public void addCourses() {
 		// will update once database connectivity is successful
 	}
+
 	public void updateSchedule() {
-		// will update once database connectivity is successful 
+		// will update once database connectivity is successful
 	}
-	
-	
-	
-	}    
 
-
+}
---
+++ src/main/java/edu/gsu/swe/aaaapp/TutorRepository.java
@@ -0,0 +1,27 @@
+package edu.gsu.swe.aaaapp;
+
+import java.util.List;
+
+public class TutorRepository {
+
+	public List<Tutor> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Tutor findById(String pantherid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void delete(Tutor tutor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Tutor save(Tutor tutor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
