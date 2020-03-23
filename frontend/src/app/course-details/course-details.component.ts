295bffaec6caef694c1da624d6cd1dac57b2a4d8
T'Avvion Jones <43850711+Halwahadi@users.noreply.github.com>
2020-03-23 01:01:43 -0400

    Add files via upload

---
+++ frontend/src/app/course-details/course-details.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../models/course';
+import { ActivatedRoute } from '@angular/router';
+import { CourseService } from '../services/course.service';
+
+@Component({
+  selector: 'app-course-details',
+  templateUrl: './course-details.component.html',
+  styleUrls: ['./course-details.component.css']
+})
+export class CourseDetailsComponent implements OnInit {
+  
+  course: Course;
+  courses: Course[];
+  id: string;
+  name: string;
+
+  constructor(private router: ActivatedRoute,
+              private courseService: CourseService) { }
+
+  ngOnInit() {
+    //id passed in the url used to get full data using the courseService
+    this.router.params.subscribe(params => {
+      this.id = params['id'];
+     // this.name = params['name'];
+      console.log(`id: ${this.id}`);
+      this.course = this.courseService.getCourse(this.id);
+      console.log(this.course);
+
+      //this.courses = this.courseService.getAllCourses();
+      //console.log(this.courses);
+    });
+  }
+}
