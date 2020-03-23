6eafba85f5396be662a6df0655c7066edf2804b4
T'Avvion Jones <43850711+Halwahadi@users.noreply.github.com>
2020-03-23 00:58:16 -0400

    Add files via upload

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,36 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginSignupComponent } from './login-signup/login-signup.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Routes, RouterModule } from '@angular/router';
+import { CourseDetailsComponent } from './course-details/course-details.component';
+import { TutorProfileComponent } from './tutor/tutor-profile/tutor-profile.component';
+import { HeaderComponent } from './nav/header/header.component';
+
+const routes : Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'courses', component: CourseListComponent, children: [
+    { path: ':id', component: CourseDetailsComponent }
+  ]},
+  { path: 'login_signup', component: LoginSignupComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginSignupComponent
+    LoginSignupComponent,
+    CourseListComponent,
+    DashboardComponent,
+    HeaderComponent,
+    CourseDetailsComponent,
+    TutorProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
